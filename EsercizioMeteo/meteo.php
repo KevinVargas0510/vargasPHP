d77cb4a346794c11e590112b3673dda05c8c39d5
Downed <kvinvar@gmail.com>
2023-02-06 18:38:53 +0100

    finito esercizio meteo

--- EsercizioMeteo/meteo.php
+++ EsercizioMeteo/meteo.php
@@ -1,56 +1,77 @@
 <?php
-  $days = array("Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato", "Domenica");
-  $weather = array("sunny", "rainy", "cloudy");
-  $city = "";
-  
-  // Check if the city was already entered by the user
-  if (isset($_COOKIE["city"])) {
+$days = array("Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato", "Domenica");
+$weather = array("sunny", "rainy", "cloudy");
+$city = "";
+$cities = array();
+
+if (isset($_COOKIE["city"])) {
     $city = $_COOKIE["city"];
-  }
-  
-  // If a new city was entered, set the cookie
-  if (isset($_POST["city"])) {
+}
+
+if (isset($_COOKIE["cities"])) {
+    $cities = json_decode($_COOKIE["cities"]);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $city = $_POST["city"];
     setcookie("city", $city, time() + 3600);
-  }
-  
-  // Generate random weather forecast for each day
-  $forecast = array();
-  for ($i = 0; $i < count($days); $i++) {
+
+    if (!in_array($city, $cities)) {
+        $cities[count($cities)] = $city;
+        setcookie("cities", json_encode($cities), time() + 3600);
+    }
+}
+
+$forecast = array();
+srand(crc32($city));
+
+for ($i = 0; $i < count($days); $i++) {
     $randomIndex = rand(0, count($weather) - 1);
     $forecast[$days[$i]] = $weather[$randomIndex];
-  }
+}
 ?>
 
 <html>
-  <head>
-    <title>Weather Forecast</title>
-  </head>
-  <body>
-    <h1>Meteo per <?= $city; ?></h1>
-    <form action="" method="post">
-      <label>Cerca località: <input type="text" name="city"></label>
-      <input type="submit" value="Cerca">
-    </form>
-    <table>
-      <tr>
-        <th>Giorno</th>
-        <th>Meteo</th>
-      </tr>
-      <?php
-        foreach ($forecast as $day => $condition) {
-          ?>
+    <head>
+        <title>Meteo</title>
+    </head>
+    <body>
+        <h1>Meteo per <?= $city; ?></h1>
+        <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+            <label>Cerca località: <input type="text" name="city"></label>
+            <input type="submit" value="Cerca">
+        </form>
+        <table>
             <tr>
-                <td>
-                    <?= $day ?>
-                </td>
-                <td>
-                    <img src="./Img/<?= $condition ?>.jpg" style="width:50px;height:50px;">
-                </td>
+                <th>Giorno</th>
+                <th>Meteo</th>
             </tr>
-          <?php
+            <?php
+            foreach ($forecast as $day => $condition) {
+                ?>
+                <tr>
+                    <td>
+                        <?= $day ?>
+                    </td>
+                    <td>
+                        <img src="./Img/<?= $condition ?>.jpg" style="width:50px;height:50px;">
+                    </td>
+                </tr>
+                <?php
+            }
+            ?>
+        </table>
+
+        <?php
+        foreach ($cities as $c) {
+            ?>
+            <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+                <input type="hidden" name="city" value="<?= $c ?>">
+                <input type="submit" value="<?= $c ?>">
+            </form>
+            <?php
         }
-      ?>
-    </table>
-  </body>
+        ?>
+
+    </body>
 </html>
